241b612cc2b0fdd386248139ebea19df15274497
Brandon Cox <brandonscottcox@gmail.com>
2022-03-16 01:32:14 -0600

    responsive page

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,29 +1,45 @@
 import React, { useState } from "react";
 import "./Home.css";
 import Header from "../Header/Header";
-import ProfilePicture from "../Images/SlackTrans.png"
-
+import ProfilePicture from "../Spring/Home/ProfilePicture";
+import BoulderMapEditTrans from "../Images/BoulderMapEditTrans.png"
 
 export default function Home() {
   const [sidebar, setSidebar] = useState(false);
 
   return (
-    <div className="HomeBackground">
-    <Header sidebar={sidebar} setSidebar={setSidebar}/>
-    <div className="CenterImg">
-
-        <div className="Test">
-          <img height="400" width="400" className="ProfilePicture" src={ProfilePicture} />
+    <>
+    <div>
+        <Header sidebar={sidebar} setSidebar={setSidebar} />
+    </div>
+      <div className="HomeBackground">
+        <div className={sidebar ? "moveImg" : "originImg"}>
+          <div className="IntroductionText">
+            <h1 className="Introduction">Brandon Cox</h1>
+            <p>
+              I'm a Frontend Developer from Boulder, Colorado.
+            </p>
+          </div>
+          <div className="ProfileImg">
+            <div className="Position">
+              <ProfilePicture />
+            </div>
+          </div>
+          <div className="flexImg">
+            <img height="450" src={BoulderMapEditTrans} />
+          </div>
+          <div className={sidebar ? "origin" : "origin"}>
+            <div>
+              <body>
+                {/* <span content="H">H</span>
+              <span content="o">o</span>
+              <span content="m">m</span>
+              <span content="e">e</span> */}
+              </body>
+            </div>
+          </div>
         </div>
-    </div>
-    <div className={sidebar ? "move" : "origin"}>
-      <body>
-        <span content="H">H</span>
-        <span content="o">o</span>
-        <span content="m">m</span>
-        <span content="e">e</span>
-      </body>
       </div>
-    </div>
+    </>
   );
 }
---
+++ frontend/src/Spring/Home/ProfilePicture.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import "../../Home/Home.css";
+import ProfilePicture from "../../Images/SlackEditTrans2.png";
+
+export default function Name() {
+  const anim = useSpring({
+    from: {
+      opacity: 0,
+    },
+    to: {
+      opacity: 1,
+    },
+    config: {
+      delay: 2000,
+      duration: 2000,
+    },
+  });
+
+  return (
+    <animated.h1 style={anim}>
+      <img
+        height="700"
+        width="700"
+        className="ProfilePicture"
+        src={ProfilePicture}
+      />
+    </animated.h1>
+  );
+}
