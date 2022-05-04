8698f2a537575f307efedc9d43e74e22b7c23c58
Brandon Cox <brandonscottcox@gmail.com>
2022-05-04 12:04:58 -0600

    fixed express

--- frontend/app.js
+++ frontend/app.js
@@ -6,35 +6,15 @@ const path = require("path");
 
 app.use(morgan("dev"));
 
-// const resumeRouter = require("./routes/resume.router")
-
-// app.use("/", resumeRouter)
-
-// const sayHello = (req, res, next) => {
-//     res.send("Hello!")
-// }
-
-// app.get("/hello", sayHello)
-
 app.use(express.static("build"));
 app.get("*", (req, res) => {
-  req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
-
-// const load = (req, res) => {
-//     req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-// }
-
-// app.get('/', load)
-// app.get("/resume", load)
 
 // if (process.env.NODE_ENV === "production") {
 //     app.use(express.static('build'))
 //     app.get('/', (req, res) => {
-//         req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-//     })
-//     app.get("/resume", (req, res) => {
-//         req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 //     })
 // }
 
@@ -44,8 +24,3 @@ app.listen(port, (err) => {
   if (err) return console.log(err);
   console.log("Server running on port: ", port);
 });
-
-// app.get("/resume", (req, res) => {
-//     console.log('Here')
-//     res.send("Hi")
-// })
