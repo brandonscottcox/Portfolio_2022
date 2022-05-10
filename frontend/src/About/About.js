bdedeaf5c9eec017993825fe12590b89bcfbc329
Brandon Cox <brandonscottcox@gmail.com>
2022-05-10 14:47:51 -0600

    all parts of the website are fully responsive, no bugs to be found

--- frontend/src/About/About.js
+++ frontend/src/About/About.js
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import "../Scrollbar.css";
 import "./About.css";
-// import ProgressBar from "react-animated-progress-bar";
-// import ReactIMG from "../Images/ReactTrans.png";
-// import CSSIMG from "../Images/CSSTrans.png";
-// import JSIMG from "../Images/JavaScriptTrans.png";
-// import HTMLIMG from "../Images/HTMLTrans.png";
 import ProgressBar from "./ProgressBarComp";
 import Thinkful from "../Images/ThinkfulWhiteTrans.png";
 import bnb from "../Images/ThinkfulbnbAbout.png";
@@ -18,45 +13,38 @@ export default function About() {
 
   return (
     <>
-      <div className="AboutBackground" id="about">
-        <div className="aboutFlex">
-          <div className="aboutSkillsFlex">
-            <div className="aboutSkillsTitle">
-              <h1>Skills</h1>
-            </div>
-            <div className="box">
-              <ProgressBar />
-            </div>
-          </div>
-          <div className="EducationBorder">
-            <div className="EducationTitle">
-              <div className="EducationHR">
-                <h1>Education</h1>
-                <hr />
-              </div>
-            </div>
-            <div></div>
-            <div className="EducationFlex">
-              <div className="ThinkfulFlex">
-                <div className="MobileImgBorder">
-                  <img className="thinkful" src={Thinkful} />
+      <div className="AboutBorder">
+        <div className="AboutBackground" id="about">
+          <div className="aboutFlex">
+            <div className="EducationBorder">
+              <div className="EducationTitle">
+                <div className="EducationHR">
+                  <h1>Education</h1>
+                  <hr />
                 </div>
-                <div>
-                  <p className="MobileHeadline">Coding Bootcamp: 2021</p>
-                  <p>
-                    Centered and formatted around full-stack web development
-                  </p>
-                  <div className="MobilePadding">
+              </div>
+              <div></div>
+              <div className="EducationFlex">
+                <div className="ThinkfulFlex">
+                  <div className="MobileImgBorder">
+                    <img className="thinkful" src={Thinkful} />
+                  </div>
+                  <div className="ThinkfulText">
+                    <p className="MobileHeadline">Coding Bootcamp: 2021</p>
                     <p>
-                      Javascript, React, CSS, HTML, PostgreSQL, Express.js,
-                      Knex.js
+                      Centered and formatted around full-stack web development
                     </p>
+                    <div className="MobilePadding">
+                      <p>
+                        Javascript, React, CSS, HTML, PostgreSQL, Express.js,
+                        Knex.js
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div id="projects" className="LinkFix"></div>
-            <div className="cardFlex">
+              <div id="projects" className="LinkFix"></div>
+              <div className="cardFlex">
                 <div className="bnbCard">
                   <a
                     style={{ textDecoration: "none" }}
@@ -76,243 +64,62 @@ export default function About() {
                     </div>
                   </a>
                 </div>
-              <div className="capstoneCard">
-                <a
-                  style={{ textDecoration: "none" }}
-                  target="_blank"
-                  href="https://bcox-front-reservation.herokuapp.com/dashboard"
-                >
-                  <div>
-                    <img src={ThinkfulCapstone} />
-                  </div>
-                  <div className="cardText">
-                    <p className="cardTitle">Final Capstone</p>
-                    <p className="cardP">
-                      Restaurant reservation application, utilizing HTTP API.
-                    </p>
-                    <p className="cardP">
-                      Technologies: React.js, Node.js, PostgreSQL, Express.js, &
-                      Knex.js
-                    </p>
-                  </div>
-                </a>
+                <div className="capstoneCard">
+                  <a
+                    style={{ textDecoration: "none" }}
+                    target="_blank"
+                    href="https://bcox-front-reservation.herokuapp.com/dashboard"
+                  >
+                    <div>
+                      <img src={ThinkfulCapstone} />
+                    </div>
+                    <div className="cardText">
+                      <p className="cardTitle">Final Capstone</p>
+                      <p className="cardP">
+                        Restaurant reservation application, utilizing HTTP API.
+                      </p>
+                      <p className="cardP">
+                        Technologies: React.js, Node.js, PostgreSQL, Express.js,
+                        & Knex.js
+                      </p>
+                    </div>
+                  </a>
+                </div>
+                <div className="FlashcardCard">
+                  <a
+                    style={{ textDecoration: "none" }}
+                    target="_blank"
+                    href="https://github.com/brandonscottcox/Project_Flashcard"
+                  >
+                    <div>
+                      <img src={Flashcard} />
+                    </div>
+                    <div className="cardText">
+                      <p className="cardTitle">Flashcard-O-Matic</p>
+                      <p className="cardP">
+                        Card based application, using bootstrap styling.
+                      </p>
+                      <p className="cardP">
+                        Technologies: React.js, Bootstrap, CSS
+                      </p>
+                    </div>
+                  </a>
+                </div>
               </div>
-              <div className="FlashcardCard">
-                <a
-                  style={{ textDecoration: "none" }}
-                  target="_blank"
-                  href="https://github.com/brandonscottcox/Project_Flashcard"
-                >
-                  <div>
-                    <img src={Flashcard} />
-                  </div>
-                  <div className="cardText">
-                    <p className="cardTitle">Flashcard-O-Matic</p>
-                    <p className="cardP">
-                      Card based application, using bootstrap styling.
-                    </p>
-                    <p className="cardP">
-                      Technologies: React.js, Bootstrap, CSS
-                    </p>
-                  </div>
-                </a>
+            </div>
+            <div className="aboutSkillsFlex">
+                <div className="aboutSkillsTitle">
+                  <h1>Skills</h1>
+                </div>
+                <div className="center">
+                <div className="box">
+                  <ProgressBar />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="AboutBackground" id="about">
-        <div className="aboutFlex">
-          <div className="aboutSkillsTitle">
-            <h1>Skills</h1>
-          </div>
-        </div>
-        <div className="aboutFlex">
-          <div className="box">
-            <div>
-              <div className="skillsImgFlex">
-                <div className="ReactIMGFlex">
-                  <img className="ReactIMG" src={ReactIMG} />
-                  <h1 className="ReactText">React</h1>
-                </div>
-              </div>
-              <div className="ProgressBar">
-                <ProgressBar
-                  width="300px"
-                  height="10px"
-                  rect
-                  fontColor="azure"
-                  percentage="70"
-                  rectPadding="1px"
-                  rectBorderRadius="20px"
-                  trackPathColor="transparent"
-                  bgColor="#333333"
-                />{" "}
-                <hr />
-              </div>
-            </div>
-            <div className="CSSTitle">
-              <div className="skillsImgFlex">
-                <div className="ReactIMGFlex">
-                  <img className="CSSIMG" src={CSSIMG} />
-                  <h1 className="CSSText">CSS</h1>
-                </div>
-              </div>
-              <div className="ProgressBar">
-                <ProgressBar
-                  width="300px"
-                  height="10px"
-                  rect
-                  fontColor="azure"
-                  percentage="75"
-                  rectPadding="1px"
-                  rectBorderRadius="20px"
-                  trackPathColor="transparent"
-                  bgColor="#333333"
-                />{" "}
-                <hr />
-              </div>
-            </div>
-            <div className="JavaScriptTitle">
-              <div className="skillsImgFlex">
-                <div className="ReactIMGFlex">
-                  <img className="JSIMG" src={JSIMG} />
-                  <h1 className="JSText">JavaScript</h1>
-                </div>
-              </div>
-              <div className="ProgressBar">
-                <ProgressBar
-                  width="300px"
-                  height="10px"
-                  rect
-                  fontColor="azure"
-                  percentage="70"
-                  rectPadding="1px"
-                  rectBorderRadius="20px"
-                  trackPathColor="transparent"
-                  bgColor="#333333"
-                />{" "}
-                <hr />
-              </div>
-            </div>
-            <div className="HTMLTitle">
-              <div className="skillsImgFlex">
-                <div className="ReactIMGFlex">
-                  <img className="HTMLIMG" src={HTMLIMG} />
-                  <h1 className="HTMLText">HTML</h1>
-                </div>
-              </div>
-              <div className="ProgressBar">
-                <ProgressBar
-                  width="300px"
-                  height="10px"
-                  rect
-                  fontColor="azure"
-                  percentage="85"
-                  rectPadding="1px"
-                  rectBorderRadius="20px"
-                  trackPathColor="transparent"
-                  bgColor="#333333"
-                />{" "}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="aboutTextFlex">
-          <div>
-            <p>
-              I've spent the past two years coding as a web developer and have
-              acquired profficient knowledge in frontend technologies such as
-              React, CSS, and HTML.
-            </p>
-          </div>
-        </div>
-        <div>
-          <div className="aboutFlex">
-            <div className="EducationTitle">
-              <h1>Education</h1>
-              <hr />
-            </div>
-          </div>
-        </div>
-        <div className="EducationFlex">
-          <div className="ThinkfulFlex">
-            <div className="MobileImgBorder">
-              <img className="thinkful" src={Thinkful} />
-            </div>
-            <div>
-              <p className="MobileHeadline">Coding Bootcamp: 2021</p>
-              <p>Centered and formatted around full-stack web development</p>
-              <div className="MobilePadding">
-                <p>
-                  Javascript, React, CSS, HTML, PostgreSQL, Express.js, Knex.js
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div id="projects" className="LinkFix"></div>
-        <div className="cardFlex">
-          <div className="bnbCard">
-            <a
-              style={{ textDecoration: "none" }}
-              target="_blank"
-              href="https://brandonscottcox.github.io/starter-thinkfulbnb/"
-            >
-              <div>
-                <img src={bnb} />
-              </div>
-              <div className="cardText">
-                <p className="cardTitle">Thinkfulbnb</p>
-                <p className="cardP">
-                  Frontend vacation rental application, focused around css
-                  styling
-                </p>
-                <p className="cardP">Technologies: React.js, CSS, HTML</p>
-              </div>
-            </a>
-          </div>
-          <div className="capstoneCard">
-            <a
-              style={{ textDecoration: "none" }}
-              target="_blank"
-              href="https://bcox-front-reservation.herokuapp.com/dashboard"
-            >
-              <div>
-                <img src={ThinkfulCapstone} />
-              </div>
-              <div className="cardText">
-                <p className="cardTitle">Final Capstone</p>
-                <p className="cardP">
-                  Restaurant reservation application, utilizing HTTP API.
-                </p>
-                <p className="cardP">
-                  Technologies: React.js, Node.js, PostgreSQL, Express.js, &
-                  Knex.js
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="FlashcardCard">
-            <a
-              style={{ textDecoration: "none" }}
-              target="_blank"
-              href="https://github.com/brandonscottcox/Project_Flashcard"
-            >
-              <div>
-                <img src={Flashcard} />
-              </div>
-              <div className="cardText">
-                <p className="cardTitle">Flashcard-O-Matic</p>
-                <p className="cardP">
-                  Card based application, using bootstrap styling.
-                </p>
-                <p className="cardP">Technologies: React.js, Bootstrap, CSS</p>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
