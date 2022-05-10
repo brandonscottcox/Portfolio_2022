69004302cd52312f0721a3b004259243c35c3685
Brandon Cox <brandonscottcox@gmail.com>
2022-05-09 21:02:10 -0600

    Contact Redesign

--- frontend/src/Contact/Contact.js
+++ frontend/src/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../Scrollbar.css";
 import "./Contact.css";
+import GoogleMap from "./GoogleMaps/GoogleMap";
 
 export default function Contact() {
   const [sidebar, setSidebar] = useState(false);
@@ -8,34 +9,43 @@ export default function Contact() {
   return (
     <>
       <div className="ContactBackground">
-        <div className="ContactFlex">
-          <div className="ContactPosition">
-            <div className="ContactText">
-              <h1>Contact</h1>
-              <div>
-                <p>I'm currently looking for future projects and clients.</p>
-                <p>Interested in getting in touch?</p>
-              </div>
-              <div className="buttonFlex">
-                <a target="_blank" href="mailto:brandonscottcox@gmail.com">
-                  <button>Email Me</button>
-                </a>
-              </div>
+        <div className="ContactPadding">
+          <div className="ContactFlex">
+            <div>
+              <GoogleMap />
             </div>
-            <div className="phoneFlex1">
-              <div className="phoneText">
-                <p>Phone: </p>
-              </div>
-              <div className="emailText">
-                <p>Email: </p>
-              </div>
-            </div>
-            <div className="phoneFlex2">
-              <div>
-                <p>+1-303-775-3638</p>
-              </div>
-              <div>
-                <p>brandonscottcox@gmail.com</p>
+            <div className="ContactTextBorder">
+              <div className="ContactPosition">
+                <div className="ContactText">
+                  <h1>Contact</h1>
+                  <div className="ContactTextP">
+                    <p>
+                      I'm currently looking for future projects and clients.
+                    </p>
+                    <p>Interested in getting in touch?</p>
+                  </div>
+                  <div className="buttonFlex">
+                    <a target="_blank" href="mailto:brandonscottcox@gmail.com">
+                      <button>Email Me</button>
+                    </a>
+                  </div>
+                  <div className="phoneFlex1">
+                    <div className="phoneText">
+                      <p>Phone: </p>
+                    </div>
+                    <div className="emailText">
+                      <p>Email: </p>
+                    </div>
+                  </div>
+                  <div className="phoneFlex2">
+                    <div>
+                      <p>+1-303-775-3638</p>
+                    </div>
+                    <div>
+                      <p>brandonscottcox@gmail.com</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
